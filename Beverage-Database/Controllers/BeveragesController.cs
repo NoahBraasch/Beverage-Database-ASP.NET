ebcfd32a636913de456821949b7305572ffb56d1
Noah Braasch <braasch.noah@gmail.com>
2022-12-15 01:14:44 -0500

    Moves from school repo

---
+++ Beverage-Database/Controllers/BeveragesController.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Beverage_Database.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+
+namespace Beverage_Database.Controllers
+{
+    [Authorize]
+    public class BeveragesController : Controller
+    {
+        private readonly BeverageContext _context;
+
+        public BeveragesController(BeverageContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Beverages
+        public async Task<IActionResult> Index()
+        {
+            DbSet<Beverage> BeveragesToFilter = _context.Beverages;
+
+            string filterName = "";
+            string filterPack = "";
+            string filterMin = "";
+            string filterMax = "";
+
+            int min = 0;
+            int max = 100;
+
+            if (!String.IsNullOrWhiteSpace(HttpContext.Session.GetString("session_name")))
+            {
+                filterName = HttpContext.Session.GetString("session_name");
+            }
+            if (!String.IsNullOrWhiteSpace(HttpContext.Session.GetString("session_pack")))
+            {
+                filterPack = HttpContext.Session.GetString("session_pack");
+            }
+            if (!String.IsNullOrWhiteSpace(HttpContext.Session.GetString("session_min")))
+            {
+                filterMin = HttpContext.Session.GetString("session_min");
+                min = Int32.Parse(filterMin);
+            }
+            if (!String.IsNullOrWhiteSpace(HttpContext.Session.GetString("session_max")))
+            {
+                filterMax = HttpContext.Session.GetString("session_max");
+                max = Int32.Parse(filterMax);
+            }
+
+            IList<Beverage> finalFiltered = await BeveragesToFilter.Where(
+                beverage => beverage.Price >= min &&
+                beverage.Price <= max &&
+                beverage.Name.Contains(filterName)
+            ).ToListAsync();
+
+            // Place string back into text box
+            ViewData["filterName"] = filterName;
+            ViewData["filterPack"] = filterPack;
+            ViewData["filterMin"] = filterMin;
+            ViewData["filterMax"] = filterMax;
+
+            return View(finalFiltered);
+
+            //Original return statement
+            //return View(await _context.Beverages.ToListAsync());
+        }
+
+        // GET: Beverages/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var beverage = await _context.Beverages
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (beverage == null)
+            {
+                return NotFound();
+            }
+
+            return View(beverage);
+        }
+
+        // GET: Beverages/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Beverages/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Pack,Price,Active")] Beverage beverage)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(beverage);
+                try
+                {
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+                catch (Exception e)
+                {
+                    ViewData["createError"] = "idError";
+                }
+            }
+            return View(beverage);
+        }
+
+        // GET: Beverages/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var beverage = await _context.Beverages.FindAsync(id);
+            if (beverage == null)
+            {
+                return NotFound();
+            }
+            return View(beverage);
+        }
+
+        // POST: Beverages/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("Id,Name,Pack,Price,Active")] Beverage beverage)
+        {
+            if (id != beverage.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(beverage);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!BeverageExists(beverage.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(beverage);
+        }
+
+        // GET: Beverages/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var beverage = await _context.Beverages
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (beverage == null)
+            {
+                return NotFound();
+            }
+
+            return View(beverage);
+        }
+
+        // POST: Beverages/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var beverage = await _context.Beverages.FindAsync(id);
+            _context.Beverages.Remove(beverage);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Filter()
+        {
+            string name = HttpContext.Request.Form["name"];
+            string pack = HttpContext.Request.Form["pack"];
+            string min = HttpContext.Request.Form["min price"];
+            string max = HttpContext.Request.Form["max price"];
+
+            HttpContext.Session.SetString("session_name", name);
+            HttpContext.Session.SetString("session_pack", pack);
+            HttpContext.Session.SetString("session_min", min);
+            HttpContext.Session.SetString("session_max", max);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool BeverageExists(string id)
+        {
+            return _context.Beverages.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Beverage-Database/Controllers/ContactController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace cis237_assignment_6.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(;
+        }
+    }
+}
